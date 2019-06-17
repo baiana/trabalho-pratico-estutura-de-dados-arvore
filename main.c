bade7e64bb1c735bf5e31af93fd1593c25bc54d4
Ana luisa <ana.luisa@cubosio>
2019-06-17 15:07:52 -0300

    add mock file and new method

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <time.h>
 
-
 typedef struct TipoNoABB *TipoApontadorABB;
 typedef struct Palavra{ char palavra[50];} Palavra;
 typedef struct arvoreInfo{
@@ -21,18 +20,6 @@ void inicializarABB(TipoApontadorABB  *arvore,ArvoreInfo *ai) {
 	 *arvore = NULL;
 	 ai->cmp = 0;
 	 ai->tempo = 0.0;
-}
-
-Palavra removeEspeciais(char* name){
-	int i = 0;
-	for(i=0; i < strlen(name);i++){
-	   	if(!(name[i]>=48 && name[i]<=57 || name[i]>=65 && name[i]<=90 || name[i]>=97 && name[i]<=122)){
-			name[i] = '\0';
-		}
-	}
-	Palavra p;
-	strcpy(p.palavra, name);
-	return p;
 }
 
 void inserirABB(Palavra x, TipoApontadorABB *arvore,ArvoreInfo *ai){
@@ -63,20 +50,54 @@ void imprimeABB(TipoApontadorABB arv){
 		imprimeABB(arv->dir);
 	}
 }
+
+Palavra removeEspeciais(char* name){
+	Palavra p;
+	int i = 0;
+	for(i=0; i < strlen(name);i++){
+	   	if(!(name[i]>=48 && name[i]<=57 || name[i]>=65 && name[i]<=90 || name[i]>=97 && name[i]<=122)){
+			name[i] = ' ';
+		}
+	}
+	strcpy(p.palavra, name);
+	return p;
+}
+
  
 void finalizaTempo(time_t inicio){
 	time_t fim = time(NULL); 
 	printf("\n Tempo do programa %f segundos", difftime(fim, inicio));
 	//https://www.techiedelight.com/find-execution-time-c-program/
-} 
+}
+
+void imprimeArvores(ArvoreInfo abb, ArvoreInfo heap, ArvoreInfo avl){
+	printf("heap:\n");
+	imprimeInfo(heap);
+	printf("abb:\n");
+	imprimeInfo(avl);
+	printf("avl:\n");
+	imprimeInfo(avl);
+	double menor = avl.tempo;
+	if (abb.tempo < menor && abb.tempo < heap.tempo && menor < heap.tempo){
+		
+	} else if(heap.tempo < menor && heap.tempo < abb.tempo && menor < abb.tempo){
+		//heap é menor
+	} else {
+		// avl é menor
+	}
+}
+
+void imprimeInfo(ArvoreInfo a){
+		printf("heap:\n%d comparacoes\n%.4lf\n\n",a.cmp,a.tempo);
+}
   
 int main(){
     FILE *fp;
 	char name[50];
 	int count = 0, i=0;
 	
-	char file[60];
-    scanf("%s",file);
+	char file[60] = "Untitled.txt";
+    //scanf("%s",file);
     fp = fopen(file, "r");
 	time_t inicio = time(NULL);
     
@@ -91,8 +112,7 @@ int main(){
     
     while(fscanf(fp, "%s", name ) != EOF ) {inserirABB(removeEspeciais(name),&arvoreBB,&abb);}
     fclose(fp);
- 
-    //imprimeABB(arvoreBB);
+    imprimeABB(arvoreBB);
     finalizaTempo(inicio);
     
     return 0;
