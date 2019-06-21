f1e9e30973d7a9888316ec6dd727bf54e3a28c75
AnaLuisa <analuisa104@gmail.com>
2019-06-21 13:10:22 -0300

    add avl implementation

--- main.c
+++ main.c
@@ -3,28 +3,32 @@
 #include <string.h>
 #include <time.h>
 
-typedef struct TipoNoABB *TipoApontadorABB;
+typedef struct TipoNo *TipoApontador;
 typedef struct Palavra{ char palavra[50];} Palavra;
 typedef struct arvoreInfo{
 	int cmp;
 	double tempo;	
 }ArvoreInfo;
 
-typedef struct TipoNoABB {
+typedef struct TipoNo {
 	Palavra p;
 	int qtd;
-	TipoApontadorABB esq, dir;
-} TipoNoABB;
+	TipoApontador esq, dir;
+} TipoNo;
 
-void inicializarABB(TipoApontadorABB  *arvore,ArvoreInfo *ai) {
+void imprimeInfo(ArvoreInfo a){ 
+printf("%d comparacoes\n%.4lf\n\n",a.cmp,a.tempo);
+}
+
+void inicializarABBeAVL(TipoApontador  *arvore,ArvoreInfo *ai) {
 	 *arvore = NULL;
 	 ai->cmp = 0;
 	 ai->tempo = 0.0;
 }
 
-void inserirABB(Palavra x, TipoApontadorABB *arvore,ArvoreInfo *ai){
+void inserirABB(Palavra x, TipoApontador *arvore,ArvoreInfo *ai){
 	if (*arvore == NULL){
-		*arvore = (TipoApontadorABB) malloc(sizeof(TipoNoABB));
+		*arvore = (TipoApontador) malloc(sizeof(TipoNo));
 		(*arvore)->p = x;
 		(*arvore)->qtd = 1;
 		(*arvore)->esq = NULL;
@@ -43,7 +47,7 @@ void inserirABB(Palavra x, TipoApontadorABB *arvore,ArvoreInfo *ai){
 	}
 }
 
-void imprimeABB(TipoApontadorABB arv){
+void imprimeABB(TipoApontador arv){
 	if(arv!=NULL){
  		printf("%s: %d\n",arv->p.palavra,arv->qtd);
 		imprimeABB(arv->esq);
@@ -63,10 +67,10 @@ Palavra removeEspeciais(char* name){
 	return p;
 }
 
- 
-void finalizaTempo(time_t inicio){
+void finalizaTempo(time_t inicio, ArvoreInfo *info){
 	time_t fim = time(NULL); 
-	printf("\n Tempo do programa %f segundos", difftime(fim, inicio));
+	info->tempo = difftime(fim, inicio);
+	printf("\ntempo abb: %lf\ntempo compara:%d",info->tempo,info->cmp);
 	//https://www.techiedelight.com/find-execution-time-c-program/
 }
 
@@ -74,46 +78,98 @@ void imprimeArvores(ArvoreInfo abb, ArvoreInfo heap, ArvoreInfo avl){
 	printf("heap:\n");
 	imprimeInfo(heap);
 	printf("abb:\n");
-	imprimeInfo(avl);
+	imprimeInfo(abb);
 	printf("avl:\n");
 	imprimeInfo(avl);
+	
 	double menor = avl.tempo;
+	
 	if (abb.tempo < menor && abb.tempo < heap.tempo && menor < heap.tempo){
-		
-	} else if(heap.tempo < menor && heap.tempo < abb.tempo && menor < abb.tempo){
-		//heap é menor
-	} else {
-		// avl é menor
+		//abb e o menor
+	} else if (heap.tempo < menor && heap.tempo < abb.tempo && menor < abb.tempo){
+		//heap e o menor
+	} else if (menor < heap.tempo && menor < abb.tempo) {
+		// avl e o  menor
+	}else {
+		//avl e o menor		
 	}
 }
+ 
+ void rotacaoSimplesEsq(TipoApontador *ppRaiz){
+	 TipoApontador pAux;
+	 pAux = (*ppRaiz)->pDir;
+	 (*ppRaiz)->pDir = pAux->pEsq;
+	 pAux->pEsq = (*ppRaiz);
+	 (*ppRaiz) = pAux;
+}
 
-void imprimeInfo(ArvoreInfo a){
-		printf("heap:\n%d comparacoes\n%.4lf\n\n",a.cmp,a.tempo);
+void rotacaoSimplesDir(TipoApontador *ppRaiz){
+	 TipoApontador pAux;
+	 pAux = (*ppRaiz)->pEsq;
+	 (*ppRaiz)->pEsq = pAux->pDir;
+	 pAux->pDir = (*ppRaiz);
+	 (*ppRaiz) = pAux;
 }
   
+int rotacaoDuplaEsq(TipoApontador *ppRaiz){
+	 int fbe = FB ( (*ppRaiz)->pEsq );
+	 if ( fbe > 0 ){
+	 	rotacaoSimplesDir(ppRaiz);
+	 	return 1;
+	}
+	 else if (fbe < 0 ){ 
+	 	rotacaoSimplesEsq( &((*ppRaiz)->pEsq));
+	 	rotacaoSimplesDir( ppRaiz );
+	 	return 1;
+	}
+	 return 0;
+}
+int BalancaDireita(TipoApontador *ppRaiz){
+	 int fbd = FB( (*ppRaiz)->pDir);
+	 if ( fbd < 0 ){
+	 rotacaoSimplesEsq(ppRaiz);
+	 return 1;
+	 }
+	 else if (fbd > 0 ){ 
+	 rotacaoSimplesDir( &((*ppRaiz)->pDir) );
+	 rotacaoSimplesEsq( ppRaiz );
+	 return 1;
+	 }
+	 return 0;
+}
+
+
+
 int main(){
     FILE *fp;
 	char name[50];
 	int count = 0, i=0;
 	
+	ArvoreInfo abb,avl,heap;
+    TipoApontador arvoreBB,arvoreAVL,arvoreHEAP;
+    inicializarABBeAVL(&arvoreBB,&abb);
+	
 	char file[60] = "Untitled.txt";
     //scanf("%s",file);
+    
     fp = fopen(file, "r");
 	time_t inicio = time(NULL);
-    
     if(fp == NULL) {
-        printf("Erro de leitura\n");
-        exit(1);
+		printf("Erro de leitura\n");
+    	exit(1);
     }
-    
-    ArvoreInfo abb;
-    TipoApontadorABB arvoreBB;
-    inicializarABB(&arvoreBB,&abb);
     
     while(fscanf(fp, "%s", name ) != EOF ) {inserirABB(removeEspeciais(name),&arvoreBB,&abb);}
     fclose(fp);
-    imprimeABB(arvoreBB);
-    finalizaTempo(inicio);
+    finalizaTempo(inicio, &abb);
+     
+	fp = fopen(file, "r");
+    inicializarABBeAVL(&arvoreAVL,&avl);
+    avl.tempo = time(NULL);
+//	while(fscanf(fp, "%s", name ) != EOF ) {inserirHeap(removeEspeciais(name),&arvoreAVL,&avl);}
+    fclose(fp);
+    
+	//imprimeABB(arvoreBB);
     
     return 0;
 }
